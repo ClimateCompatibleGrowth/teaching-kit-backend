cfc19b1f45aeaafccea0a31c465d1c232975bf60
Petter Tasola <petter.tk@hotmail.com>
2022-12-01 11:53:06 +0100

    Modify config to work in RDS+S3 production (#2)
    * Modify config to work in RDS+S3 production
    
    * Add readme instructions for creating psql db
    
    * Add Plugin environment condition
    
    Co-authored-by: Olof Berg Marklund <olof.marklund@frankdigital.se>

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,24 +1,23 @@
-module.exports = ({ env }) => ({
-  upload: {
-    config: {
-      provider: 'aws-s3',
-      providerOptions: {
-        accessKeyId: env('AWS_ACCESS_KEY_ID'),
-        secretAccessKey: env('AWS_ACCESS_SECRET'),
-        region: env('AWS_REGION'),
-        params: {
-          Bucket: env('AWS_BUCKET_NAME'),
+module.exports = ({ env }) =>
+  env('NODE_ENV') === 'production'
+    ? {
+      upload: {
+        config: {
+          provider: 'aws-s3',
+          providerOptions: {
+            accessKeyId: env('AWS_ACCESS_KEY_ID'),
+            secretAccessKey: env('AWS_ACCESS_SECRET'),
+            region: env('AWS_REGION'),
+            params: {
+              Bucket: env('AWS_BUCKET_NAME'),
+            },
+          },
+          actionOptions: {
+            upload: {},
+            uploadStream: {},
+            delete: {},
+          },
         },
       },
-      // These parameters could solve issues with ACL public-read access — see [this issue](https://github.com/strapi/strapi/issues/5868) for details
-      actionOptions: {
-        upload: {
-          ACL: null
-        },
-        uploadStream: {
-          ACL: null
-        },
-      }
-    },
-  }
-});
+    }
+    : {}
