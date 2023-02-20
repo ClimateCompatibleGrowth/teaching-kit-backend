4af2bbd482c44409ad0eec88bbf4c6a53519ac76
Petter Tasola <petter.tk@hotmail.com>
2023-02-20 09:44:23 +0100

    Add controllers to find block+lecture+course on vuid (instead of id) (#14)

--- src/api/block/controllers/block.ts
+++ src/api/block/controllers/block.ts
@@ -4,4 +4,52 @@
 
 import { factories } from '@strapi/strapi'
 
-export default factories.createCoreController('api::block.block');
+type Block = {
+  id: number
+  vuid: string
+  isVisibleInListView: boolean
+  publishedAt: null | string
+}
+
+export default factories.createCoreController(
+  'api::block.block',
+  ({ strapi }) => ({
+    async findByVuid(ctx) {
+      try {
+        const { vuid } = ctx.params
+        const { populate } = ctx.query
+
+        const entries: Block[] = await strapi.entityService.findMany(
+          'api::block.block',
+          {
+            filters: {
+              vuid,
+            },
+            populate,
+          }
+        )
+
+        if (entries === undefined || entries.length === 0) {
+          ctx.status = 404
+          ctx.body = `Unable to find block with vuid ${vuid}`
+          return
+        }
+
+        const entry = entries.find(
+          (entry) => entry.isVisibleInListView && entry.publishedAt !== null
+        )
+
+        if (entry === undefined) {
+          ctx.body = 404
+          ctx.body = `Unable to find a published block with vuid ${vuid}`
+        }
+
+        const sanitizedEntry = await this.sanitizeOutput(entry, ctx)
+        ctx.body = sanitizedEntry
+      } catch (error) {
+        ctx.status = 500
+        ctx.body = error
+      }
+    },
+  })
+)
--- src/api/course/controllers/course.ts
+++ src/api/course/controllers/course.ts
@@ -4,4 +4,52 @@
 
 import { factories } from '@strapi/strapi'
 
-export default factories.createCoreController('api::course.course');
+type Course = {
+  id: number
+  vuid: string
+  isVisibleInListView: boolean
+  publishedAt: null | string
+}
+
+export default factories.createCoreController(
+  'api::course.course',
+  ({ strapi }) => ({
+    async findByVuid(ctx) {
+      try {
+        const { vuid } = ctx.params
+        const { populate } = ctx.query
+
+        const entries: Course[] = await strapi.entityService.findMany(
+          'api::course.course',
+          {
+            filters: {
+              vuid,
+            },
+            populate,
+          }
+        )
+
+        if (entries === undefined || entries.length === 0) {
+          ctx.status = 404
+          ctx.body = `Unable to find course with vuid ${vuid}`
+          return
+        }
+
+        const entry = entries.find(
+          (entry) => entry.isVisibleInListView && entry.publishedAt !== null
+        )
+
+        if (entry === undefined) {
+          ctx.body = 404
+          ctx.body = `Unable to find a published course with vuid ${vuid}`
+        }
+
+        const sanitizedEntry = await this.sanitizeOutput(entry, ctx)
+        ctx.body = sanitizedEntry
+      } catch (error) {
+        ctx.status = 500
+        ctx.body = error
+      }
+    },
+  })
+)
