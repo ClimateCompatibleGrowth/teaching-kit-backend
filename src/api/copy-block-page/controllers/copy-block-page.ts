9b2e2cf35ed33d9b5c021ffd715bf2906a65e668
RickardssonJ <48394514+RickardssonJ@users.noreply.github.com>
2023-03-16 11:26:31 +0100

    add copy to landing pages (#27)

---
+++ src/api/copy-block-page/controllers/copy-block-page.ts
@@ -0,0 +1,7 @@
+/**
+ * copy-block-page controller
+ */
+
+import { factories } from '@strapi/strapi'
+
+export default factories.createCoreController('api::copy-block-page.copy-block-page');
---
+++ src/api/copy-course-page/controllers/copy-course-page.ts
@@ -0,0 +1,7 @@
+/**
+ * copy-course-page controller
+ */
+
+import { factories } from '@strapi/strapi'
+
+export default factories.createCoreController('api::copy-course-page.copy-course-page');
