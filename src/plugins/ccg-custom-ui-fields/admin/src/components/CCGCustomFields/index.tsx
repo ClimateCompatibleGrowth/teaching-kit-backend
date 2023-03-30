067d38d590787c2e1d38f60c23d6c90330f93307
tasola <petter.tk@hotmail.com>
2023-03-30 13:11:26 +0200

    Add possibility to re-attempt fetching zenodo link to CCGCustomFields

--- src/plugins/ccg-custom-ui-fields/admin/src/components/CCGCustomFields/index.tsx
+++ src/plugins/ccg-custom-ui-fields/admin/src/components/CCGCustomFields/index.tsx
@@ -8,12 +8,24 @@ const GRAY = 'rgb(165, 165, 186)'
 type Entry = {
   id: string
   zenodo_deposit_id: string | null
+  zenodo_doi: string | null
 }
 
+// Note: Since Strapi doesn't allow requests to http-domains (which the Curriculum website of course is locally), this
+// is a bit challengin to test locally.
+// What I've done is to hard code the request to fetch some combination of vuid/version from production which I know
+// exists in production.
+// A slightly more cumbersome approach (but also better) is to use https tunneling with e.g ngrok (https://ngrok.com/). In that
+// case you must edit the CORS configuration of the get-zenodo-deposit endpoint to accept your custom ngrok https-domain in development.
+
+// For some reason, the entry you're at when browsing this plugin is automatically saved when you click the button to look for a new
+// Zenodo link. I really can't understand why this happens. If anyone is curious in the future, someone has hopefully answered this
+// forum post: https://forum.strapi.io/t/custom-plugin-entry-is-automatically-saved-when-performing-api-call/27079
 const CCGCustomFields = () => {
-  const [linkToZenodoDeposit, setLinkToZenodoDeposit] = React.useState<
-    string | undefined
-  >(undefined)
+  const [isLoading, setIsLoading] = React.useState(false)
+  const [zenodoEntry, setZenodoEntry] = React.useState<Entry | undefined>(
+    undefined
+  )
   const { initialData } = useCMEditViewDataManager()
 
   const dataIsLearningMaterial = initialData?.Abstract !== undefined
@@ -23,17 +35,22 @@ const CCGCustomFields = () => {
   }, [initialData?.vuid, initialData?.versionNumber])
 
   const fetchDepositDatabaseEntry = async (vuid: string, version: number) => {
+    setIsLoading(true)
     try {
       const response = await fetch(
         `${process.env.NEXT_ENDPOINT}/get-zenodo-deposit?secret=${process.env.GET_ZENODO_SECRET}&vuid=${vuid}&version=${version}`,
         { method: 'GET' }
       )
-      console.log(response)
       const matchingEntry: Entry = await response.json()
-      const link = `https://zenodo.org/deposit/${matchingEntry.zenodo_deposit_id}`
-      setLinkToZenodoDeposit(link)
+      if (!matchingEntry.zenodo_deposit_id) {
+        setIsLoading(false)
+        return setZenodoEntry(undefined)
+      }
+      setZenodoEntry(matchingEntry)
     } catch (error) {
       console.error(error)
+    } finally {
+      setIsLoading(false)
     }
   }
 
@@ -79,9 +96,28 @@ const CCGCustomFields = () => {
         <Typography textColor="rgb(165, 165, 186)" fontWeight="bold">
           Zenodo deposit
         </Typography>
-        {linkToZenodoDeposit !== undefined ? (
-          <Typography textAlign="right">{linkToZenodoDeposit}</Typography>
-        ) : null}
+        {zenodoEntry?.zenodo_deposit_id !== undefined &&
+        zenodoEntry?.zenodo_doi !== undefined ? (
+          <Typography textAlign="right">
+            <a
+              href={`https://zenodo.org/deposit/${zenodoEntry.zenodo_deposit_id}`}
+              target="_blank"
+            >
+              {zenodoEntry.zenodo_doi}
+            </a>
+          </Typography>
+        ) : (
+          <button
+            onClick={() =>
+              fetchDepositDatabaseEntry(
+                initialData?.vuid,
+                initialData?.versionNumber
+              )
+            }
+          >
+            {isLoading ? 'Loading...' : 'Check for Zenodo link'}
+          </button>
+        )}
       </div>
     </div>
   )
